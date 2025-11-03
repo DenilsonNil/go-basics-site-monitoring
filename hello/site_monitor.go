a5bdba05b7431f9af0dec474913e1889e3eb9243
d.alves <d.alves@unlimint.com>
2025-11-03 19:48:45 -0300

    Add site_monitor.go

---
+++ hello/site_monitor.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	showIntroduction()
+	showMenu()
+	option := readOption()
+	processOption(option)
+
+	/**
+	if option == 1 {
+		fmt.Println("Starting monitoring...")
+	} else if option == 2 {
+		fmt.Println("Showing logs...")
+	} else if option == 0 {
+		fmt.Println("Exiting...")
+	} else {
+		fmt.Println("Invalid option. Please try again.")
+	}
+		**/
+
+}
+
+func showIntroduction() {
+	userName := "Denilson"
+	fmt.Println("========================================")
+	fmt.Println("Hello", userName, "!Welcome to our site monitor. Choose some options below:")
+	fmt.Println("========================================")
+}
+
+func showMenu() {
+	fmt.Println("1. Start Monitoring")
+	fmt.Println("2. Show Logs")
+	fmt.Println("0. Exit")
+
+	fmt.Println("========================================")
+}
+
+func readOption() int {
+	var option int
+	fmt.Scanf("%d", &option)
+	fmt.Println("You chose option:", option)
+	return option
+}
+
+func processOption(option int) {
+	switch option {
+	case 1:
+		fmt.Println("Starting monitoring...")
+	case 2:
+		fmt.Println("Showing logs...")
+	case 0:
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid option. Please try again.")
+		os.Exit(-1)
+	}
+}
